9c76516f19719518121239abdbd2120e38c1a256
filipe custodio <filipe.l.custodio@gmail.com>
2019-07-14 15:41:16 +0100

    Adding two simple functional components to test React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>
+      <Link to="/pagetwo">Navigate to page Two.</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      <button>Click me!</button>
+      <Link to="/">Navigate to page One.</Link>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>Twitch clone</div>;
-}
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;

0c13e5709dbb581b6361345bd52409aace88dc4b
filipe custodio <filipe.l.custodio@gmail.com>
2019-07-18 23:26:16 +0100

    Saving gauth user id in redux store since it will be needed later on for crud operations

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -18,7 +18,7 @@ class GoogleAuth extends React.Component {
 
   onAuthChange = (isSignedIn) => {
     if (isSignedIn) {
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
@@ -58,6 +58,7 @@ class GoogleAuth extends React.Component {
 }
 
 const mapStateToProps = (state) => {
+  console.log(state);
   return { isSignedIn: state.auth.isSignedIn }
 }
 

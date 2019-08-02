2869e0afab0794e06f9ac02c4ee5bab3ad51fb1c
filipe custodio <filipe.l.custodio@gmail.com>
2019-08-02 23:37:51 +0100

    implementing programmatic Navigation by taking control of the history object in react by replacing BrowserRouter with Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './Header';
-import StreamCreate from './streams/StreamCreate';
-import StreamDelete from './streams/StreamDelete';
-import StreamEdit from './streams/StreamEdit';
-import StreamList from './streams/StreamList';
-import StreamShow from './streams/StreamShow';
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import Header from "./Header";
+import createBrowserHistory from "../history";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
-      <Header />
+      <Router history={createBrowserHistory}>
+        <Header />
         <div>
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
@@ -19,7 +20,7 @@ const App = () => {
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
